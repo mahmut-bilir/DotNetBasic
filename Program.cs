aa7888388ab6abe1b70fcc9d63d9d3f607547ff5
Mahmut Bilir <mahmut-bilir@hotmail.com>
2025-03-17 21:50:22 +0300

    Example of operators

--- Program.cs
+++ Program.cs
@@ -6,3 +6,7 @@ double pi = 3.14;
 char letter = 'A';
 bool control = true;
 string message = "Hello, World!";
+var name = "Mahmut";
+var age = 18;
+int x = 10, y = 20;
+bool result = (x < y) && (y > 15);
