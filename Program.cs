83ff650f80afa52a4a9464aacc573075149cdebf
Mahmut Bilir <mahmut-bilir@hotmail.com>
2025-03-17 21:41:58 +0300

    Variable definition

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int number = 5;
+double pi = 3.14;
+char letter = 'A';
+bool control = true;
+string message = "Hello, World!";
